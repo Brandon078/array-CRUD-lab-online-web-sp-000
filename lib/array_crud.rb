4ff514695b85a83f09552bb12b97929339196866
brandon moro <brandon_078@hotmail.com>
2019-11-20 21:44:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  array = ["yo", "sup"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array << "Go go gadget"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("hey")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop("hey")
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift("yo")
 end
 
 def retrieve_element_from_index(array, index_number)
