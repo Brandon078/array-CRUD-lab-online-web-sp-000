ca596630c01a8cd2ebabfd1ea790de33fb850bea
brandon moro <brandon_078@hotmail.com>
2019-11-20 21:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["yo", "sup", "fit", "cute"]
-  array.push("Go go gadget")
+  new_array.push("Go go gadget")
 end
 
 def add_element_to_start_of_array(array, element)
